2d0222c7c7a5a4e3f68906be3bb62f3d9e29056a
Vinod Srinivasan <whitewolf33@gmail.com>
2016-11-27 00:07:57 +1100

    Rounded Boxview Pageview Indicator Work Experience Carousel View

---
+++ Common/Converters/PageIndicatorToColorConverter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Common
+{
+	public class PageIndicatorToColorConverter : IValueConverter
+	{
+		public Color SuccessColor { get; set; } = Color.Green;
+		public Color FailureColor { get; set; } = Color.Transparent;
+
+		public int PageIndex
+		{
+			get; set;
+		}
+
+
+		#region IValueConverter implementation
+
+		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			int currentIndicator;
+			int boxViewIndex = 0;
+
+			if (parameter != null && parameter is Binding)
+			{
+				var source = ((Binding)parameter).Source;
+				if (source != null && source is IPage)
+				{
+					boxViewIndex = ((IPage)source).Index;
+				}
+			}
+
+			Int32.TryParse(value.ToString(), out currentIndicator);
+			//Int32.TryParse(parameter.ToString(), out boxViewIndex);
+
+			if (currentIndicator > 0 && boxViewIndex > 0)
+			{
+				if (currentIndicator <= boxViewIndex)
+					return SuccessColor;
+			}
+			return FailureColor;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+	}
+}
---
+++ Common/Interfaces/IPage.cs
@@ -0,0 +1,7 @@
+﻿namespace Common
+{
+	public interface IPage
+	{
+		int Index { get; set; }
+	}
+}
