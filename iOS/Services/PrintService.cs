3fedc5c2163d999a125cfdfb393a2e3f925db50e
Vinod Srinivasan <whitewolf33@gmail.com>
2017-05-25 06:59:16 +1000

    Printing resume done

--- iOS/Services/PrintService.cs
+++ iOS/Services/PrintService.cs
@@ -13,36 +13,14 @@ namespace Resume.iOS
 	{
 		public void Print()
 		{
-			
-			var fullPath =Environment.GetFolderPath(Environment.SpecialFolder.Personal) + "/resume.pdf";
-			bool exists = File.Exists(fullPath);
-
-			if (exists)
-			{
-				var pdf = NSData.FromFile(fullPath);
-				// Get a reference to the singleton iOS printing concierge
-				UIPrintInteractionController printController = UIPrintInteractionController.SharedPrintController;
-				printController.PrintingItem = pdf;
-				// Instruct the printing concierge to use our custom UIPrintPageRenderer subclass when printing this job
-				printController.PrintPageRenderer = new PrintPageRenderer();
-
-				// Ask for a print job object and configure its settings to tailor the print request
-				UIPrintInfo info = UIPrintInfo.PrintInfo;
-
-				// B&W or color, normal quality output for mixed text, graphics, and images
-				info.OutputType = UIPrintInfoOutputType.General;
-
-				// Select the job named this in the printer queue to cancel our print request.
-				info.JobName = "Resume";
-
-				// Instruct the printing concierge to use our custom print job settings.
-				printController.PrintInfo = info;
-
-				// Present the standard iOS Print Panel that allows you to pick the target Printer, number of pages, double-sided, etc.
-
-				printController.Present(true, PrintingCompleted);
-			}
-
+			UIPrintInteractionController printController = UIPrintInteractionController.SharedPrintController;
+			printController.PrintingItem = NSUrl.FromString("https://vinodresume.azurewebsites.net/resume/");
+			printController.ShowsPageRange = true;
+			UIPrintInfo info = UIPrintInfo.PrintInfo;
+			info.OutputType = UIPrintInfoOutputType.General;
+			info.JobName = "Resume";
+			printController.PrintInfo = info;
+			printController.Present(true, PrintingCompleted);
 		}
 
 		void PrintingCompleted(UIPrintInteractionController controller, bool completed, NSError error)
