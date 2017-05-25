969fa76309382f0347079453ca56f7f3784f7f2c
Vinod Srinivasan <whitewolf33@gmail.com>
2017-05-26 08:05:02 +1000

    About Page realignment Provisioning Profile

---
+++ Common/Controls/ListViewExt.cs
@@ -0,0 +1,19 @@
+﻿using Xamarin.Forms;
+
+namespace Common
+{
+	public class ListViewExt : ListView
+	{
+		public ListViewExt()
+		{
+			// This is to ensure that the selection does not remain on the screen
+			this.ItemTapped += (sender, e) =>
+			{
+				if (e.Item != null)
+				{
+					((ListView)sender).SelectedItem = null;
+				}
+			};
+		}
+	}
+}
